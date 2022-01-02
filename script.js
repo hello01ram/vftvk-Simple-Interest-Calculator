1a0a5cef1f0bba1077d3c7e6c8ddf64c4143194e
hello01ram <r.r.alsaifi@gmail.com>
2022-01-02 21:26:27 +1100

    Update visual rate value on change

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById('principal').value;
+    var rate = document.getElementById('rates').value;
+    var year = document.getElementById('year').value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+   
 }
-        
+
+function updateRate() {
+    var rateVal = document.getElementById('rate').value;
+    document.getElementById('rate_val').innerText = rateVal;
+}
